a0484cb84ea06727b7e137edeaada33e9c680780
Laila1567 <149065010+Laila1567@users.noreply.github.com>
2024-12-04 09:31:19 -0300

    Update Item.java

--- Trabalho_ED2/Item.java
+++ Trabalho_ED2/Item.java
@@ -1,16 +1,20 @@
 package Trabalho_ED2;
+/*
+Discentes: Laila Bianca, Luís Henrique, Marcus luis
+Data de codificação: 16/12/2024
+Nome da disciplina: Estrutura de dados II
+
+Objetivo da classe: Resgatar valores para serem usados como argumento em outras classes
+como "Ordena" e "TesteOrdena".
+*/
 
 public class Item {
-
     // Atributo que armazena o valor ligado ao objeto Item
     private int valor;
-
-    // Construtor que inicializa o atributo 'valor' com o valor passado como
-    // argumento
+    // Construtor que inicializa o atributo 'valor' com o valor passado como argumento
     public Item(int valor) {
         this.valor = valor;
     }
-
     // Metodos getter e setters
     public int getValor() {
         return valor;
@@ -18,6 +22,10 @@ public class Item {
 
     public void setValor(int valor) {
         this.valor = valor;
+    }
+}
+
+alor;
     }
 
 }
