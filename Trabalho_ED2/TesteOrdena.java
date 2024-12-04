b8a5486cf5b70166607edfdf1750e72c6d51072a
Laila1567 <149065010+Laila1567@users.noreply.github.com>
2024-12-04 09:43:58 -0300

    Update TesteOrdena.java

--- Trabalho_ED2/TesteOrdena.java
+++ Trabalho_ED2/TesteOrdena.java
@@ -1,103 +1,117 @@
-package Trabalho_ED2;
+package trabalho2_ED2;
+
+/*
+Discentes: Laila Bianca, Luís Henrique, Marcus Luan
+Data de codificação: 16/08/2024
+Nome da disciplina: Estrutura de dados II
+
+Objetivo da classe: Executar algoritmos de ordenação e calcular
+tempos de execução com cópias de um mesmo vetor.
+ */
 
 public class TesteOrdena {
-    public static void main(String[] args) {
-        int execucoes = 100;
+	public static void main(String[] args) {
+		int execucoes = 100;
 
-        // Gerar os datasets
-        Item[][] dataset1 = Utilidades.geraDataset1();
-        Item[][] dataset2 = Utilidades.geraDataset2();
+		// Gerar os datasets
+		Item[][] dataset1 = Utilidades.geraDataset1();
+		Item[][] dataset2 = Utilidades.geraDataset2();
 
-        // Medir o tempo para o Dataset1
-        System.out.println("\nMedição para Dataset1:\n");
-        medirTempo(dataset1, execucoes);
+		// Medir o tempo para o Dataset1
+		System.out.println("\nMedição para Dataset1:\n");
+		medirTempo(dataset1, execucoes);
 
-        // Medir o tempo para o Dataset2
-        System.out.println("\nMedição para Dataset2:\n");
-        medirTempo(dataset2, execucoes);
-    }
+	}
 
-    // Método para medir o tempo de execução dos algoritmos
-    public static void medirTempo(Item[][] dataset, int execucoes) {
-        // Repete sobre cada vetor de Item no dataset
-        for (int y = 0; y <= 2; y++) {
-            
-        	if (y == 0) {
-                System.out.println("=== Heap sort ===");
-                for (Item[] vetor : dataset) {
-                    // Inicializa a variável que vai acumular o tempo total de execução
-                    long somaTempo = 0;
+	// Método para medir o tempo de execução dos algoritmos
+	public static void medirTempo(Item[][] dataset, int execucoes) {
+		// Repete sobre cada vetor de Item no dataset
+		for (int y = 2; y == 2; y++) {
+			//int[] tamanhos = {10, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+	        int[] tamanhos = {1000, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000};
 
-                    for (int i = 0; i < execucoes; i++) {
-                        // Cria uma cópia do vetor para garantir que o mesmo vetor seja usado em cada execução
 
-                        Item[] vetorCopia = copiarVetor(vetor);
+			if (y == 0) {
+				System.out.println("=== Heap sort ===");
+				
+				//int[] tamanhos = {10, 20};
+				for (int tam : tamanhos) {
+					// Inicializa a variável que vai acumular o tempo total de execução
+					long somaTempo = 0;
+					Item[] vetor = null;
+					for (int i = 0; i < execucoes; i++) {
 
-                        long tempInicio = System.nanoTime(); // Captura o tempo inicial
-                        Ordena.heapSort(vetorCopia, y);
-                        long tempFim = System.nanoTime(); // Captura o tempo final
+						vetor = Utilidades.geraDataset(tam);
 
-                        long tempoExecucao = tempFim - tempInicio;
+						long tempInicio = System.nanoTime(); // Captura o tempo inicial
+						Ordena.heapSort(vetor, vetor.length);
+						long tempFim = System.nanoTime(); // Captura o tempo final
 
-                        // Acumula o tempo de execução em uma variável que armazena a soma total dos tempos
+						long tempoExecucao = tempFim - tempInicio;
 
-                        somaTempo += tempoExecucao;
-                    }
+						// Acumula o tempo de execução em uma variável que armazena a soma total dos tempos
 
-                    long media = somaTempo / execucoes;
-                    System.out.printf("Tamanho do vetor: %d, Tempo médio: %d nanosegundos\n", vetor.length, media);
-                }
-            }
-            if (y == 1) {
-                System.out.println("=== Insertion sort ===");
-                for (Item[] vetor : dataset) {
-                    long somaTempo = 0;
+						somaTempo += tempoExecucao;
+					}
 
-                    for (int i = 0; i < execucoes; i++) {
-                        Item[] vetorCopia = copiarVetor(vetor);
-                        long tempInicio = System.nanoTime();
-                        Ordena.insertionSort(vetorCopia);
-                        long tempFim = System.nanoTime();
-                        long tempoExecucao = tempFim - tempInicio;
-                        somaTempo += tempoExecucao;
-                    }
+					long media = somaTempo / execucoes;
+					System.out.printf("Tamanho do vetor: %d, Tempo médio: %d nanosegundos\n", vetor.length, media);
+				}
+			}
+			if (y == 1) {
+				System.out.println("=== Insertion sort ===");
+				for (int tam : tamanhos) {
+					// Inicializa a variável que vai acumular o tempo total de execução
+					long somaTempo = 0;
+					Item[] vetor = null;
+					for (int i = 0; i < execucoes; i++) {
 
-                    long media = somaTempo / execucoes;
-                    System.out.printf("Tamanho do vetor: %d, Tempo médio: %d nanosegundos\n", vetor.length, media);
-                }
-            }
-            if (y == 2) {
-                System.out.println("=== Quick sort ===");
-                for (Item[] vetor : dataset) {
-                    long somaTempo = 0;
-                    for (int i = 0; i < execucoes; i++) {
-                        Item[] vetorCopia = copiarVetor(vetor);
-                        long tempInicio = System.nanoTime();
-                        Ordena.quicksort(vetorCopia);
-                        long tempFim = System.nanoTime();
-                        long tempoExecucao = tempFim - tempInicio;
+						vetor = Utilidades.geraDataset(tam);
 
-                        somaTempo += tempoExecucao;
-                    }
+						long tempInicio = System.nanoTime(); // Captura o tempo inicial
+						Ordena.insertionSort(vetor);
+						long tempFim = System.nanoTime(); // Captura o tempo final
 
-                    long media = somaTempo / execucoes;
-                    System.out.printf("Tamanho do vetor: %d, Tempo médio: %d nanosegundos\n", vetor.length, media);
-                }
-            }
-        }
-    }
-   
-    public static Item[] copiarVetor(Item[] vetorOriginal) {
-        // Cria um novo vetor com o mesmo comprimento do vetor original
-        Item[] copia = new Item[vetorOriginal.length];
+						long tempoExecucao = tempFim - tempInicio;
 
-        // Itera sobre cada elemento do vetor original
-        for (int i = 0; i < vetorOriginal.length; i++) {
+						// Acumula o tempo de execução em uma variável que armazena a soma total dos tempos
 
-            // Cria um novo objeto Item para cada elemento do vetor original e copia o valor do Item original para o novo Item
-            copia[i] = new Item(vetorOriginal[i].getValor());
-        }
-        // Retorna a cópia do vetor
-        return copia;
-    }   
-}
+						somaTempo += tempoExecucao;
+					}
+
+					long media = somaTempo / execucoes;
+					System.out.printf("Tamanho do vetor: %d, Tempo médio: %d nanosegundos\n", vetor.length, media);
+				}
+			}
+			if (y == 2) {
+				System.out.println("=== Quick sort ===");
+				for (int tam : tamanhos) {
+					// Inicializa a variável que vai acumular o tempo total de execução
+					long somaTempo = 0;
+					Item[] vetor = null;
+					for (int i = 0; i < execucoes; i++) {
+						
+						vetor = Utilidades.geraDataset(tam);
+
+						long tempInicio = System.nanoTime(); // Captura o tempo inicial
+						Ordena.quicksort(vetor);
+						long tempFim = System.nanoTime(); // Captura o tempo final
+
+						long tempoExecucao = tempFim - tempInicio;
+
+						// Acumula o tempo de execução em uma variável que armazena a soma total dos tempos
+
+						somaTempo += tempoExecucao;
+					}
+
+
+					long media = somaTempo / execucoes;
+					System.out.printf("Tamanho do vetor: %d, Tempo médio: %d nanosegundos\n", vetor.length, media);
+				}
+			}
+		}
+	}
+
+	}
+	
+
